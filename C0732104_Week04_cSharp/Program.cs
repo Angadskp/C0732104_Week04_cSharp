92a18f687a5375507436c89a56edf8ba8131a77f
visualstudio <visualstudio@122-PC05>
2019-02-01 20:43:37 -0500

    8:40 committ

--- C0732104_Week04_cSharp/Program.cs
+++ C0732104_Week04_cSharp/Program.cs
@@ -10,7 +10,54 @@ namespace C0732104_Week04_cSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+        // ounntryside c = new countryside();
+        //  c.run();
+        
+        }
+    }
+    class LearningExperment {
+        public void run() {
+
+            Village Toronto;
+            Toronto = new Village();
+            Village a, b, c;
+            Toronto.villageName = "Version A";
+            a = Toronto;
+            Console.WriteLine(a.villageName);
+            Toronto = new Village();
+            Toronto.villageName = "Version B";
+            b = Toronto;
+            Console.WriteLine(b.villageName);
+
+            Toronto = new Village();
+            Toronto.villageName = "Version C";
+            c = Toronto;
+            Console.WriteLine(c.villageName);
+
+        }
+    }
+
+    class Village
+    {
+       public bool isAstrilde;
+        public Village nextVillage;
+        public Village prevVillage;
+        public string villageName;
+
+    }
+
+    class Countryside
+    {
+        Village Toronto, Maple, Ajax;
+
+        public void run()
+        {
+            Maple = new Village();
+            Toronto = new Village();
+            Ajax = new Village();
+            Maple.villageName = "Toronto";
+            Maple.nextVillage = Toronto;
+            
         }
     }
 }
